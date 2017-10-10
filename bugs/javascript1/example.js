49378b3670645dff13dc6986eefd88a5153fb6c3
Frank Versnel <f.versnel@gmail.com>
2017-10-10 21:15:18 +0200

    Forgot to remove file.

--- bugs/javascript1/example.js
+++
@@ -1,9 +0,0 @@
-// This program should print:
-// hackyourfuture
-
-const elements = ["hack", "your", "future"];
-let result = "";
-for (let i = 0; i <= elements.length; i++) {
-    result += elements[i];
-}
-console.log(result);
